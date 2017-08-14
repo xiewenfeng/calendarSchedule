1ad9e8405a21d538cf6ca19cf9fc773fc376b2ef
xiewenfeng <smileiam@163.com>
2017-08-14 15:09:49 +0800

    移除无用代码

--- DefinedView/calendar/src/main/java/com/smile/calendar/baseadapter/OnItemClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.smile.calendar.baseadapter;
-
-/**
- * Created by jingbin on 2016/3/2.
- */
-public interface OnItemClickListener<T> {
-    void onClick(T t, int position);
-}
--- DefinedView/calendar/src/main/java/com/smile/calendar/baseadapter/OnItemLongClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.smile.calendar.baseadapter;
-
-/**
- * Created by jingbin on 16/7/4.
- */
-public interface OnItemLongClickListener<T> {
-    void onLongClick(T t, int position);
-}
